71a505c34e8f9453d73ee6ae4e447e41611d4af9
KumarDevada <kumardevada123@gmail.com>
2023-05-22 22:01:12 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './style.scss'
+const App = () => {
 
-function App() {
+  const [data,setData] = useState('');
+  const [year,setYear] = useState(0);
+  const [month,setMonth] = useState(0);
+  const [message,setMessage] = useState('none');
+
+  
+  const calculate = () => {
+    if(data === '') {
+      alert('Please Enter Your Date of Birth');
+      return ;
+    }
+    var s = data.split('-');
+    var today = new Date()
+    var y = (today.getFullYear() - s[0]);
+    var m = (today.getMonth()+1 - s[1])
+    console.log(y + 'years');
+    console.log(today.getMonth()+ 'today month');
+    console.log(m + 'months');
+    if(y<0) {
+      setMessage('Future Kid');
+    }
+    else if(y===0) {
+      if(m<0) {
+        setMessage('Future Kid');
+      }
+      else if(m===0) {
+        setMessage('New Born Baby')
+      }
+      else {
+        setMessage('none')
+      }
+    }
+    else {
+      if(m<0) {
+        y=y-1;
+        m=12+m;
+      }
+      setMessage('none')
+    }
+
+    setYear(y)
+    setMonth(m)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <center>
+      <div className='container'>
+        <h2><span>Know</span> Your <span>Age</span></h2>
+        <input type='date' onChange={e => setData(e.target.value)}></input>
+        <button onClick={e=>calculate()}>Calculate</button>
+        {message==='none' && 
+          <div>
+            {year!==0 && <p>{year} Years</p>}
+            {month!==0 && <p>{month} Months</p>}
+          </div>}
+        {message!=='none' && <p>{message}</p>}
+      </div>
+      
+    </center>
+    
+  )
 }
 
-export default App;
+export default App
